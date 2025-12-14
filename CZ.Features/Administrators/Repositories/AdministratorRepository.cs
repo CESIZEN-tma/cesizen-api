15584d7404a10c1155822706b374e2a41d74d8e7
Titouan Massé <titouanmasselaude@gmail.com>
2025-12-14 15:50:28 +0100

    Added result pattern and doc about it

--- CZ.Features/Administrators/Repositories/AdministratorRepository.cs
+++ CZ.Features/Administrators/Repositories/AdministratorRepository.cs
@@ -4,28 +4,5 @@ namespace api.CZ.Features.Administrators.Repositories;
 
 public class AdministratorRepository : IAdministratorRepository
 {
-    public async Task<IEnumerable<Administrator>> GetAllAdministratorsAsync()
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<Administrator?> GetAdministratorByIdAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<Administrator> CreateAdministratorAsync(Administrator administrator)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<Administrator?> UpdateAdministratorAsync(int id, Administrator administrator)
-    {
-        throw new NotImplementedException();
-    }
-
-    public async Task<bool> DeleteAdministratorAsync(int id)
-    {
-        throw new NotImplementedException();
-    }
+    
 }
--- CZ.Features/Administrators/Repositories/IAdministratorRepository.cs
+++ CZ.Features/Administrators/Repositories/IAdministratorRepository.cs
@@ -4,9 +4,4 @@ namespace api.CZ.Features.Administrators.Repositories;
 
 public interface IAdministratorRepository
 {
-    Task<IEnumerable<Administrator>> GetAllAdministratorsAsync();
-    Task<Administrator?> GetAdministratorByIdAsync(int id);
-    Task<Administrator> CreateAdministratorAsync(Administrator administrator);
-    Task<Administrator?> UpdateAdministratorAsync(int id, Administrator administrator);
-    Task<bool> DeleteAdministratorAsync(int id);
 }
